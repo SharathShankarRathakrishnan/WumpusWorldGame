70be4b707fe0ca50705baa15324baa1c99852ebe
SharathShankarRathakrishnan <SharathShankarRathakrishnan@users.noreply.github.com>
2026-05-31 05:38:56 +0000

    deploy: 41b4856da397a4a4115601ec5d1de33bc817cadd

--- coi-register.js
+++
@@ -1,7 +0,0 @@
-if (typeof SharedArrayBuffer === 'undefined') {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js').then(() => {
-      if (!navigator.serviceWorker.controller) window.location.reload();
-    });
-  }
-}
